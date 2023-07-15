2167a0d4b317411fa065216d9705f22c6189d54f
lioness3 <joann333carter@gmail.com>
2023-07-14 21:25:16 -0400

    fix footer routing on index.html to use partial

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var hbs = require('hbs'); // https://www.npmjs.com/package/hbs
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
+const travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
