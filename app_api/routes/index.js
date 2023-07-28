135fc64ded6053f2e4051d0e676369fb581abaf2
lioness3 <joann333carter@gmail.com>
2023-07-27 22:08:27 -0400

    create single trip api route

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 const tripsController = require('../controllers/trips');
 
-/* GET htrips list. */
+/* GET trips list. */
 router
     .route('/trips')
     .get(tripsController.tripsList);
+
+    /* GET single trip. */
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindCode);
 
 module.exports = router;
